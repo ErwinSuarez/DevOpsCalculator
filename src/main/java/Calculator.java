c8659fe6b1aecd961c658973c29b129710211bf1
Erwin <erwinsuarez2003@yahoo.com>
2019-05-03 12:20:02 +0100

    changes in Calculator class to get code coverage

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,25 +1,12 @@
 class Calculator {
-
     double multiply(double x, double y){
-        if((x==0) || (y==0))
-            return 0;
-        else
-            return x*y;
+        if((x==0) || (y==0)){ return 0; }
+        else{ return x*y;}
     }
-
     double divide(double x, double y) throws ArithmeticException {
-        if(x==0)
-            return 0;
-        else
-            return x/y;
+        if(x==0){ return 0;}
+        else{ return x/y; }
     }
-
-    double addition(double x, double y){
-        return x+y;
-    }
-
-    double subtraction(double x, double y){
-        return x-y;
-    }
-
+    double addition(double x, double y){ return x+y; }
+    double subtraction(double x, double y){ return x-y; }
 }
