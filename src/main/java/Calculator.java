2156ab212d9ed5a66f68dbb1428beb74b8e8dfb4
Erwin <erwinsuarez2003@yahoo.com>
2019-05-03 02:47:37 +0100

    created Calculator class in main and CalculatorTest in test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,35 @@
 class Calculator {
 
+    public double multiply(double x, double y){
+        if((x==0) || (y==0)){
+            return 0;
+        }
+        else{
+            return x*y;
+        }
+    }
+
+    public double divide(double x, double y){
+        if(x==0){
+            return 0;
+        }
+        else{
+            double div = 0;
+            try{
+                div = x/y;
+            }catch (ArithmeticException e){
+                System.out.println("ArithmeticException occured!");
+            }
+            return div;
+        }
+    }
+
+    public double addition(double x, double y){
+        return x+y;
+    }
+
+    public double subtraction(double x, double y){
+        return x-y;
+    }
+
 }
