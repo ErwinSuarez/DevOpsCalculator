1f360a427ccbe466592ad1263a1b6dcb389ebd37
Erwin <erwinsuarez2003@yahoo.com>
2019-05-03 02:53:06 +0100

    added 4 methods in Calculator main class added 4 test in CalculatorTest in test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,26 @@
-public class CalculatorTest {
-    public final Calculator calc = new Calculator();
+import static org.junit.jupiter.api.Assertions.*;
 
+class CalculatorTest {
+
+    private Calculator calc = new Calculator();
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+        assertEquals(calc.multiply(3,4), 12);
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+        assertEquals(calc.divide(18,3), 6);
+    }
+
+    @org.junit.jupiter.api.Test
+    void addition() {
+        assertEquals(calc.addition(2,7), 9);
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtraction() {
+        assertEquals(calc.subtraction(13, 6), 7);
+    }
 }
